eeb6100c9b558b18b96741c4a33e938ef77b5c40
fyrineholm <kaizholm@gmail.com>
2025-04-19 02:00:36 +0100

    add my page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,html}"],
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx,html}",
+		"./public/**/*.{js,ts,jsx,tsx,html}",
+	],
 	theme: {
 		extend: {},
 	},
